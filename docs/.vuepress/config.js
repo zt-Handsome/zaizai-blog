a3ca4bfaaf38a92c634fff356b8d0a9e3e336b4d
zhangt <zhangt@polysoft.com.cn>
2022-03-03 08:45:34 +0800

    pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   title: "俊劫的学习基地",
-  base: '/', // 格式：'/<部署仓库名>/'， 默认'/'
+  base: '/zaizai-blog/', // 格式：'/<部署仓库名>/'， 默认'/'
   description: "俊劫的学习基地是俊劫的个人博客，用于记录学习笔记、分享音乐、书籍、旅行等个人兴趣的站点。",
   dest: "public",
   theme: require.resolve('../../theme-reco-vdoing'), // 使用本地主题
